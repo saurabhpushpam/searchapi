2e9b5813d8ff4a636c7cd71fe06be6bb15c89de3
Saurabh Pushpam <saurabhpushpam3@gmail.com>
2023-10-30 14:42:15 +0530

    commit

---
+++ models/productModel.js
@@ -0,0 +1,22 @@
+var mongoose= require("mongoose");
+
+const productSchema= mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+
+    description:{
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: String,
+        required: true
+    }
+
+});
+
+
+module.exports= mongoose.model("product", productSchema);
